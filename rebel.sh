e296aa4ae1b376bf1dd1e04424b152f510f3ee6b
Ramana Venkata <idlike2dream@gmail.com>
2016-09-12 23:59:58 +0530

    Rebel Binary now builds from npm packages only

--- rebel.sh
+++ rebel.sh
@@ -1,18 +1,5 @@
-# Convert reason files to ocaml files
-# TODO probably should use a for loop
-
 PROJECT=$1
 
 rm -r $PROJECT/_build
-
-FILES="utils setup bucklescript rebel npmDep native"
-
-for file in $FILES
-do
-  rm -f src/$file.ml && refmt -print ml -parse re src/$file.re >> src/$file.ml
-done
-
-# recompile oasis just in case _oasis is changed
-oasis setup && ./configure && make
-cp -f ./rebel.native $PROJECT/node_modules/.bin/rebel && \
+cp -f ./rebel $PROJECT/node_modules/.bin/rebel
 cd $PROJECT && npm start
