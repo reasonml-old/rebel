c6a64d6c1f59da5d0f9e383b249e684d8a783dbb
Ramana Venkata <idlike2dream@gmail.com>
2016-09-15 03:49:20 +0530

    remove the ocamlopt script

--- compile-rebel.sh
+++ compile-rebel.sh
@@ -9,14 +9,11 @@ echo $selfSortedFiles
 for source in $selfSortedFiles
 do
   destination=$(echo $source | sed "s/src/_build\/src/" | sed "s/\.re$//")
-  ocamlfind ocamlopt -g -bin-annot -pp refmt -w "-40-30" -thread -package threads -package ppx_let -package core -package jenga -package yojson -o $destination -I _build/src -c -impl $source
+  ocamlfind ocamlc -custom -g -bin-annot -pp refmt -w "-40-30" -thread -package threads -package ppx_let -package core -package jenga -package yojson -o $destination -I _build/src -c -impl $source
 done
 
 
-selfSortedArtifactsO=$(echo $selfSortedFiles | sed "s/src/_build\/src/g" | sed "s/\.re/\.cmo/g")
-selfSortedArtifactsX=$(echo $selfSortedFiles | sed "s/src/_build\/src/g" | sed "s/\.re/\.cmx/g")
+selfSortedArtifacts=$(echo $selfSortedFiles | sed "s/src/_build\/src/g" | sed "s/\.re/\.cmo/g")
 # should give: _build/self/myDep.cmo _build/self/myDep2.cmo _build/self/test.cmo
 # ocamlfind ocamlc -o _build/src/rebel -thread -package threads -package ppx_let -package core -package jenga -package yojson -I _build/src $selfSortedArtifactsO
-ocamlfind ocamlopt -o _build/src/rebel -linkpkg -thread -package threads -package ppx_let -package core -package jenga -package yojson -I _build/src $selfSortedArtifactsX
-
-
+ocamlfind ocamlc -custom -o _build/src/rebel -linkpkg -thread -package threads -package ppx_let -package core -package jenga -package yojson -I _build/src $selfSortedArtifacts
